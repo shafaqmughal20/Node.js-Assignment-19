529c923710296683aa52b9fd72e7defec476d68b
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:47:38 +0500

    Add files via upload

---
+++ Q-19.js
@@ -0,0 +1,6 @@
+"use strict";
+//19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let guestArray = ["Ahmed", "Hamza", "Usama"];
+//guestArray.map((item)=> console.log(`Dear ${item} you are invited to the dinner`))
+let lengthGuest = guestArray.length;
+console.log(`we are inviting ${lengthGuest} guests.`);
---
+++ Q-19.ts
@@ -0,0 +1,8 @@
+//19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestArray:string[]=["Ahmed","Hamza","Usama"]
+//guestArray.map((item)=> console.log(`Dear ${item} you are invited to the dinner`))
+
+
+let lengthGuest:number=guestArray.length
+console.log(`we are inviting ${lengthGuest} guests.`)
